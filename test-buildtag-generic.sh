7b54d1c29fb0ae6c8bd72511532fd121e59d373c
Andrew Collins <ancollin@redhat.com>
2026-08-26 16:07:35 -0700

    IBMCloud runs as an example
    Signed-off-by: Andrew Collins <ancollin@redhat.com>

--- test-buildtag-generic.sh
+++ test-buildtag-generic.sh
@@ -11,4 +11,4 @@ echo
 DEBUG=--debug
 DISPLAY="--display buildTag,masterNodesType,workerNodesCount,fips,benchmark,buildUrl"
 
-orion $DEBUG $DISPLAY --hunter-analyze --build-tag 2085683329012600832 --config examples/generic.yaml
+uv run main.py $DEBUG $DISPLAY --hunter-analyze --build-tag 2089779365775675392 --build-tag 2090401610168537088 --build-tag 2090481448787120128 --build-tag 2089779365834395648 --build-tag 2090401610235645952 --build-tag 2090481451307896832 --build-tag 2090481455502200832 --config examples/generic.yaml
